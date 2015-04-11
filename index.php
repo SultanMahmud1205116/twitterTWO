7bf2837118fe24cdef10340256f8a1ff016cd9c9
Sultan <1205116.sm@ugrad.cse.buet.ac.bd>
2015-04-11 23:43:36 +0600

    this is the premier league version

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
  
 $requestMethod = "GET";
  
-$getfield = '?screen_name=iagdotme&count=20';
+$getfield = '?screen_name=premierleague&count=20';
  
 $twitter = new TwitterAPIExchange($settings);
 echo $twitter->setGetfield($getfield)
